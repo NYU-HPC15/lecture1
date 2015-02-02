f9efa93bed03a1748a69efed0fb06970f12bc984
Georg Stadler <georgst@ices.utexas.edu>
2015-02-02 15:27:51 -0500

    fix the mpi-version of inner product

--- inner-mpi.c
+++ inner-mpi.c
@@ -22,7 +22,7 @@ int main (int argc, char **argv)
 
   if (argc != 3) {
     fprintf(stderr, "Function needs vector size and number of passes as input arguments!\n");
-    abort();
+    MPI_Abort(MPI_COMM_WORLD,1);
   }
 
   n = atol(argv[1]);
@@ -44,8 +44,8 @@ int main (int argc, char **argv)
 
   /* fill vectors */
   for (i = 0; i < my_n; ++i) {
-    a[i] = offset + i;
-    b[i] = 2.*(offset + i);
+    a[i] = 1 + offset + i;
+    b[i] = 2./(offset + i + 1);
   }
 
   timestamp_type time1, time2;
@@ -53,26 +53,27 @@ int main (int argc, char **argv)
 
   for (p = 0; p < passes; ++p)
     {
-      for (i = 0; i < n; ++i) {
+      for (i = 0; i < my_n; ++i) {
 	c[i] = a[i] * b[i];
       }
 
       my_prod = 0.0;
-      for (i = 0; i < n; ++i) {
+      for (i = 0; i < my_n; ++i) {
       	my_prod += c[i];
       }
-      MPI_Reduce(&my_prod, &prod, mpisize, MPI_LONG, MPI_SUM,
-		 mpirank, MPI_COMM_WORLD);
+      MPI_Reduce(&my_prod, &prod, 1, MPI_DOUBLE, MPI_SUM,
+		 0, MPI_COMM_WORLD);
     }
 
   get_timestamp(&time2);
   double elapsed = timestamp_diff_in_seconds(time1,time2);
-  printf("Time elapsed is %f seconds.\n", elapsed);
-  printf("Inner product is %f.\n", prod);
+  if (mpirank == 0) {
+    printf("Time elapsed is %f seconds.\n", elapsed);
+    printf("Inner product is %f.\n", prod);
 
-  printf("%f GB/s\n", 4*n*sizeof(double)*passes/1e9/elapsed);
-  printf("%f GFlops/s\n", 2*n*passes/1e9/elapsed);
-
+    printf("%f GB/s\n", 4*n*sizeof(double)*passes/1e9/elapsed);
+    printf("%f GFlops/s\n", 2*n*passes/1e9/elapsed);
+  }
   free(a);
   free(b);
   free(c);
